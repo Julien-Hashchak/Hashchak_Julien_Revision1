28c243d9aa8db5f3b8ac1afe296ddb17ebdea152
Julien-Hashchak <2369740@cegepmontpetit.ca>
2025-03-13 14:35:12 -0400

    fdfsfsd

--- S06-Revision-Trucs-Cours2-main/Assets/Scripts/VariationMushroom.cs
+++ S06-Revision-Trucs-Cours2-main/Assets/Scripts/VariationMushroom.cs
@@ -5,18 +5,45 @@ using UnityEngine;
 public class VariationMushroom : MonoBehaviour
 {
 
-    [SerializeField] float _distanceMaximale;
+    [SerializeField] private float _distanceMaximale;
 
     // Start is called before the first frame update
     void Start()
     {
-        valeur = Random.value(0f,1f);
         Vector3 nouvellePosition = Vector3.zero;
+
+        Vector2 variation = Random.insideUnitCircle * _distanceMaximale;
+
+        nouvellePosition.x = variation.x;
+        nouvellePosition.y = variation.y;
+
+        transform.position += nouvellePosition;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class VariationPositionAleatoire : MonoBehaviour
+// {
+//     [SerializeField] private float _distanceMaximale;
+
+//     void Start()
+//     {
+//         // Declarer et initialiser une variable locale
+//         Vector3 nouvellePosition = Vector3.zero;
+
+//         // Calculer une position aleatoire a partir d'un Vector2
+//         // � une distance maximale
+//         Vector2 variation = Random.insideUnitCircle * _distanceMaximale;
+        
+//         // Convertir les valeurs du Vector2 (x,y) au Vector3 (x,z)
+//         nouvellePosition.x = variation.x;
+//         nouvellePosition.z = variation.y;
+        
+//         // Appliquer la nouvelle position
+//         transform.position += nouvellePosition;
+//     }
+// }
